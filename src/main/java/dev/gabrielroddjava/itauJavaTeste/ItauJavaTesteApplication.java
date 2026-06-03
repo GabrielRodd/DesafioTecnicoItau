7ad3fe83a80867493c10b4d390d37edaf4670e28
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-06-02 21:47:19 -0300

    First Commit

---
+++ src/main/java/dev/gabrielroddjava/itauJavaTeste/ItauJavaTesteApplication.java
@@ -0,0 +1,13 @@
+package dev.gabrielroddjava.itauJavaTeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauJavaTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauJavaTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gabrielroddjava/itauJavaTeste/ItauJavaTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielroddjava.itauJavaTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauJavaTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
