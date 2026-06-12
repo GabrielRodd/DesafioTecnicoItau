f741662e58a6a6c155344a6d944e59dcbe4e30a3
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-06-11 22:07:24 -0300

    feat: Adiciona o EstatisticaDTO como abstracao de Model

---
+++ src/main/java/dev/gabrielroddjava/itauJavaTeste/Estatistica/EstatisticaDTO.java
@@ -0,0 +1,18 @@
+package dev.gabrielroddjava.itauJavaTeste.Estatistica;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EstatisticaDTO {
+
+    private Long count;
+    private Double sum;
+    private Double avg;
+    private Double min;
+    private Double max;
+
+}
