e787dc24d88fd13b8b75b5ce7a0505376eb63b59
GabrielRodd <gabriel.rdgs33@outlook.com>
2026-06-23 11:07:56 -0300

    refactor: Inclui a anotacao de validated para impedir que os segundos de retorno das estatisticas seja menor ou igual a zero

--- src/main/java/dev/gabrielroddjava/itauJavaTeste/Estatistica/EstatisticaProperties.java
+++ src/main/java/dev/gabrielroddjava/itauJavaTeste/Estatistica/EstatisticaProperties.java
@@ -1,8 +1,11 @@
 package dev.gabrielroddjava.itauJavaTeste.Estatistica;
 
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticaProperties(Integer segundos) {
+@Validated
+public record EstatisticaProperties(@Positive Integer segundos) {
 }
